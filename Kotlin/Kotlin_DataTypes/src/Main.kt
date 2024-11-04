726ccfbdfede65ee86ba3437c7fdad91b81d926f
Barsat khadka <khadkabarsat598@gmail.com>
2024-11-04 13:47:20 -0600

    Kotlin File

---
+++ Kotlin/Kotlin_DataTypes/src/Main.kt
@@ -0,0 +1,18 @@
+fun main(){
+//    print("Print without ln does not change lines")
+//    println(" Welcome to kotlin")
+//    print("Now its changed")
+
+//    var a = 56
+//    var b = 56
+//    print("The total sum of $a + $b is ${a + b}")
+
+//    var firstName = "Barsat"
+//    var lastName = "Khadka"
+//
+//    var fullName = firstName+ lastName
+//    println("My full name is $fullName")
+//
+//    println("My full name is ${firstName} ${lastName}")
+
+}
