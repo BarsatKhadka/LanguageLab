0e80703593224142bb6109965c8d5176850b70a4
Barsat khadka <khadkabarsat598@gmail.com>
2024-11-21 17:27:01 -0600

    Docker Practice

---
+++ Docker/java-project/Test.java
@@ -0,0 +1,9 @@
+class Test{
+    public static void printHere(){
+        System.out.println("This is proper printing method");
+
+    }
+    public static void main(String[] args) {
+        System.out.println("This is test of the century");
+    }
+}
